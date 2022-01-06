c8d345469255bda6f539ff0d934da6ecc47e612f
XenderMd <denis.stavilamd@gmail.com>
2022-01-06 12:38:27 +0100

    worked with some syntax

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
@@ -6,11 +7,23 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
+            var numbers = new []{12.7, 10.3, 6.11};
+            var grades = new List<double>(){12.7, 10.3, 6.11}; 
+            var gradesNum = grades.Count;
+            double result = 0.0;
+     
             if(args.Length>0){
                 Console.WriteLine($"Hello {args[0]}!");
             }else{
                 Console.WriteLine("Hello world!");
-            }
+            };
+
+            foreach(double number in grades){
+                result+=number;
+            };
+            result/=gradesNum;
+
+            Console.WriteLine($"Your average grade is {result:N3}");
         }
     };
 };
