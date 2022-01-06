0507fb21b6e5f09186d3c045b32086ae1de3fad4
XenderMd <denis.stavilamd@gmail.com>
2022-01-06 13:38:52 +0100

    Implemented statistics in the Book class

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -1,12 +1,38 @@
+using System;
+using System.Collections.Generic;
+
 namespace GradeBook {
 
       class Book{
 
-          
+        private List<double> grades;
+        private string name;
+        private double highGrade;
+        private double lowGrade;
 
-          public void AddGrade(double grade){
+        public Book(string name){
+            this.name = name;
+            grades = new List<double>();
+            highGrade=double.MinValue;
+            lowGrade=double.MaxValue;
+        }
 
-          }
+        public void AddGrade(double grade){
+            grades.Add(grade);
+        }
+        
+        public void ShowStatistics(){
+            CalculateStatistics();
+            Console.WriteLine($"The lowest grade is {lowGrade:N3}");
+            Console.WriteLine($"The highest grade is {highGrade:N3}");
+        }
+
+        private void CalculateStatistics(){
+            foreach(var grade in grades){
+                highGrade = Math.Max(highGrade, grade);
+                lowGrade=Math.Min(lowGrade, grade);
+            };
+        }
     };
 
 };
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -7,19 +7,11 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            var book = new Book();
-            book.AddGrade(15.5);
-
-            var grades = new List<double>(){12.7, 10.3, 6.11}; 
-            var gradesNum = grades.Count;
-            double result = 0.0;
-    
-            foreach(double number in grades){
-                result+=number;
-            };
-            result/=gradesNum;
-
-            Console.WriteLine($"Your average grade is {result:N3}");
+            var book = new Book("Denis's Grade Book");
+            book.AddGrade(12.7);
+            book.AddGrade(10.3);
+            book.AddGrade(6.11);
+            book.ShowStatistics();
         }
     };
 };
