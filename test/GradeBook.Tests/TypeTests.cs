0d6d8e61348485c2b5c26e3bd1b665874bdcabfc
XenderMd <denis.stavilamd@gmail.com>
2022-01-06 16:33:54 +0100

    Finished implementing type tests -> reference types vs value types

--- test/GradeBook.Tests/TypeTests.cs
+++ test/GradeBook.Tests/TypeTests.cs
@@ -5,8 +5,32 @@ namespace GradeBook.Tests
 {
     public class TypeTests
     {
-        Book GetBook(string name){
+        private Book GetBook(string name){
             return new Book(name);
+        }
+
+        private void GetBookSetName(Book book, string name){
+            book = new Book(name);
+        }
+
+        private void GetBookSetName(ref Book book, string name){
+            book = new Book(name);
+        }
+
+        private void SetName(Book book, string name){
+            book.Name=name;
+        }
+
+        private int GetInt(){
+            return 3;
+        }
+
+        private void SetInt(ref int x){
+            x=42;
+        }
+
+        private string MakeUppercase(string parameter){
+            return parameter.ToUpper();
         }
 
         [Fact]
@@ -30,12 +54,78 @@ namespace GradeBook.Tests
             var book1=GetBook("Book 1");
             var book2=book1;
 
-            //Act
-               
             //Assert
             Assert.Same(book1, book2);
             Assert.True(Object.ReferenceEquals(book1, book2));
         }
 
+        [Fact]
+        public void CanSetNameFromReference()
+        {
+           //Arrange
+           var book1=GetBook("Book 1");
+           
+           //Act
+            SetName(book1, "New Name");
+               
+            //Assert
+            Assert.Equal("New Name", book1.Name);
+        }
+
+        [Fact]
+        public void CSharpIsPassByValue()
+        {
+           //Arrange
+           var book1=GetBook("Book 1");
+           
+           //Act
+            GetBookSetName(book1, "New Name");
+               
+            //Assert
+            Assert.Equal("Book 1", book1.Name);
+        }
+
+        [Fact]
+          public void CSharpCanPassByRef()
+        {
+           //Arrange
+           var book1=GetBook("Book 1");
+           
+           //Act
+            GetBookSetName(ref book1, "New Name");
+               
+            //Assert
+            Assert.Equal("New Name", book1.Name);
+        }
+
+        [Fact]
+        public void Test1(){
+            
+            //Arrange
+            var x = GetInt();
+            
+            //Assert
+            Assert.Equal(3, x);
+
+            SetInt(ref x);
+            
+            //Assert
+            Assert.Equal(42, x);
+
+        }
+
+        [Fact]
+        public void StringsBehaveLikeValueTypes(){
+            
+            //Arrange
+            string name = "Denis";
+
+            //Act
+            string upper = MakeUppercase(name);
+
+            //Assert
+            Assert.Equal("Denis", name);
+            Assert.Equal("DENIS", upper);
+        }
     }
 };
